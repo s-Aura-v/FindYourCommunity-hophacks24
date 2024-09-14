e70520f8213a8f2529d7b556a061d8f4aeb30067
sonephyo <sonephyo7777777@gmail.com>
2024-09-13 21:41:52 -0400

    init: HopHacks2024 init

---
+++ FindMyCommunityBackend/src/main/java/com/Hop_Hacks2024/FindMyCommunityBackend/FindMyCommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Hop_Hacks2024.FindMyCommunityBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyCommunityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyCommunityBackendApplication.class, args);
+	}
+
+}
---
+++ FindMyCommunityBackend/src/test/java/com/Hop_Hacks2024/FindMyCommunityBackend/FindMyCommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hop_Hacks2024.FindMyCommunityBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyCommunityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
